164893fcccc69c95826eee1ca19514d606346e91
Stanislas-Web <stanislasmakengo1@gmail.com>
2020-12-01 12:42:59 +0100

    integration de l'image

--- src/components/Bannier.jsx
+++ src/components/Bannier.jsx
@@ -2,15 +2,25 @@ import styled from 'styled-components';
 import img from '../assets/bg.jpg'
 
 const BannierStyle = styled.div`
+display:flex;
+justify-content: center;
+align-items: center;
 height: 500px;
-background-size: cover;
+background-repeat: no-repeat;
+background-size: auto;
 background-image: url(${img});
 `;
 
+const H1Style = styled.h1`
+// font-size: 15px;
+font-weight: 300;
+font-family: "roboto", Helvetica Neue, Helvetica, sans-serif;
+color: white;
+`;
 
 function Bannier() {
     return  <BannierStyle>
-                
+                <H1Style> Nous rapprochons les entrepreneurs et investisseurs </H1Style>
             </BannierStyle>
   }
   
